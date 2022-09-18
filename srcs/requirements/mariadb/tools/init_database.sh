9f56cf81e696da7b8c2b55559a607aa845ed160a
Charles Mariot <contact@charles-mariot.fr>
2022-09-18 03:05:37 +0200

    WP Auth key generation [18 Sep 2022 at 03:05]

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 
-/etc/init.d/mariadb setup
-rc-service mariadb start
-
+secure_database() {
 cat << EOF | mysql_secure_installation
 
 Y
@@ -12,15 +10,23 @@ Y
 Y
 Y
 EOF
+}
 
+
+create_database() {
 cat << EOF | mariadb
 create database $MYSQL_DATABASE;
 grant all privileges on $MYSQL_DATABASE.* TO "$MYSQL_USER"@'localhost' identified by "$MYSQL_PASSWORD";
 grant all privileges on $MYSQL_DATABASE.* TO "$MYSQL_USER"@'%' identified by "$MYSQL_PASSWORD";
 flush privileges;
 EOF
+}
 
+
+/etc/init.d/mariadb setup
+rc-service mariadb start
+secure_database
+create_database
 rc-service mariadb restart
 rc-service mariadb stop
-
 exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/tools/wordpress_istall.sh
+++ srcs/requirements/wordpress/tools/wordpress_istall.sh
@@ -6,13 +6,41 @@
 #    By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/07 14:15:26 by cmariot           #+#    #+#              #
-#    Updated: 2022/09/15 15:16:17 by cmariot          ###   ########.fr        #
+#    Updated: 2022/09/18 01:32:47 by cmariot          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #! /bin/sh
 
 WORDPRESS_CONFIG=/var/www/wordpress/
+
+# Download wordpress and untar it
+download_wordpress()
+{
+	cd /var/www
+	wget http://wordpress.org/latest.tar.gz
+	tar -xvzf latest.tar.gz
+	rm latest.tar.gz
+}
+
+update_wp_config_files()
+{
+	cd /var/www/wordpress
+	# Configuration of wp-config.php
+	sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${MYSQL_DATABASE}' );/g"		wp-config-sample.php
+	sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${MYSQL_USER}' );/g"				wp-config-sample.php
+	sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );/g"	wp-config-sample.php
+	sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306' );/g"						wp-config-sample.php
+	# Change Authentification unique keys
+	wget -O salts.txt https://api.wordpress.org/secret-key/1.1/salt/
+	head -50 wp-config-sample.php > tmp_file.php
+	cat salts.txt >> tmp_file.php
+	tail -38 wp-config-sample.php >> tmp_file.php
+	echo "define('FS_METHOD', 'direct');" >> tmp_file.php
+	sed 's/\r//' tmp_file.php > wp-config.php
+	rm -f tmp_file.php salts.txt wp-config-sample.php
+}
+
 
 if [ ! -z "$(ls -A $WORDPRESS_CONFIG)" ];
 then
@@ -22,27 +50,8 @@ then
 else
 
 	echo "Wordpress installation ..."
-
-	cd /var/www
-
-	# Download wordpress and untar it
-	wget http://wordpress.org/latest.tar.gz
-	tar -xvzf latest.tar.gz
-	rm latest.tar.gz
-
-	cd wordpress
-
-	# Configuration of wp-config.php
-	sed -i "s/define( 'DB_NAME', 'database_name_here' );/define( 'DB_NAME', '${MYSQL_DATABASE}' );/g"		wp-config-sample.php
-	sed -i "s/define( 'DB_USER', 'username_here' );/define( 'DB_USER', '${MYSQL_USER}' );/g"				wp-config-sample.php
-	sed -i "s/define( 'DB_PASSWORD', 'password_here' );/define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );/g"	wp-config-sample.php
-	sed -i "s/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', 'mariadb:3306' );/g"						wp-config-sample.php
-
-
-	# Change Authentification unique keys
-
-	mv wp-config-sample.php wp-config.php
-	
+	download_wordpress
+	update_wp_config_files
 	echo "The WordPress installation is completed."
 
 fi
