fd1b16f6a62baf0d1978aa0d353fcab9102dc0a9
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-09-21 20:16:32 +0200

    Bonus directory moved to requierements

--- srcs/requirements/bonus/adminer/tools/adminer_install.sh
+++ srcs/requirements/bonus/adminer/tools/adminer_install.sh
@@ -5,5 +5,5 @@ if [ ! -f "/var/www/wordpress/adminer.php" ];
 then
 	# Install adminer
 	cd /var/www/wordpress
-	wget -O "adminer.php" "https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php"
+	wget -O "adminer.php" "https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php"
 fi
