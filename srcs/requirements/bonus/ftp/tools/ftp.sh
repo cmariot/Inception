2fd1ae6015a73c8d012522ca0f01ce1afdee76ce
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-09-22 20:57:36 +0200

    FTP server

---
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -0,0 +1,18 @@
+adduser -D $FTP_USER && cat << EOF | passwd $FTP_USER
+$FTP_PASSWORD
+$FTP_PASSWORD
+EOF
+
+echo $FTP_USER >> /etc/vsftpd/vsftpd_user_conf
+
+mkdir -p /var/www/worpdress &&\
+	chown -R $FTP_USER:$FTP_USER /var/www/wordpress
+
+mkdir -p /etc/ssl/certs &&\
+	cd /etc/ssl/certs &&\
+	openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
+	-keyout vsftpd.key -out vsftpd.pem \
+	-subj "/C=FR/ST=Paris/L=Paris/O=42/OU=student/CN=cmariot"
+	chmod 600 vsftpd.pem
+
+exec vsftpd /etc/vsftpd/vsftpd.conf
